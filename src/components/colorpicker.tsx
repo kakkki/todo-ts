32fd1679b59b93834387ca298d04f7a01785b8f5
kakehi-binc <kakehi@binc.jp>
2020-09-06 16:29:08 +0900

    ColorBrowser, ColorPickerを実装

---
+++ src/components/colorpicker.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Color } from "../model/color";
+
+interface Props {
+  color: Color;
+  onColorUpdated: (color: Color) => void;
+}
+
+export const ColorPicker = (props: Props) => (
+  <div  >
+    <input
+      type="range"
+      min="0"
+      max="255"
+      value={props.color.red}
+      onChange={event =>
+        props.onColorUpdated({
+          red: +event.target.value,
+          green: props.color.green,
+          blue: props.color.blue
+        })
+      }
+    />
+    red : {props.color.red}
+  </div>
+);
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { TaskInput } from "./TaskInput";
 export { TaskItem } from "./TaskItem";
 export { TaskList } from "./TaskList";
-
+export { ColorBrowser } from "./colorBrowser";
+export { ColorPicker } from "./colorpicker";
